6db62436ef670692c5a84d6d9dee747398ae0b57
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-08-02 21:42:52 -0300

    avanço!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicosService } from './service/servicos.service';
 import { Subscription } from 'rxjs/Subscription';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,42 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
+
   dados: any;
+  serie = '';
+  strikes: any[] = [];
+
 
   constructor(private servicos: ServicosService) {
   }
 
   ngOnInit() {
+  }
+  tabChanged = (tabChangeEvent: MatTabChangeEvent): void => {
+    // console.log('tabChangeEvent => ', tabChangeEvent);
+    // console.log('index => ', tabChangeEvent.index);
+     if (tabChangeEvent.tab.textLabel === 'Calendar Butterfly') {
+        this.calendarButerfly();
+     }
+  }
+
+  calendarButerfly() {
     this.servicos.getCalendarButerfly().subscribe(d => {
       this.dados = d;
+
+      this.serie = d[0]['Serie'];
       console.log('Dados: ', this.dados);
+      console.log('Serie: ', this.serie);
+      this.filtrarStrike(this.dados);
     });
+  }
+
+  filtrarStrike(strike) {
+        strike.filter(s => {
+          this.strikes.push(s.asa_inferior.Strike);
+          this.strikes.push(s.miolo.Strike);
+          this.strikes.push(s.asa_superior.Strike);
+        });
   }
 
 }
---
+++ src/app/models/calendar-butterfly.ts
@@ -0,0 +1,4 @@
+export interface CalendarButterfly {
+        $key: string;
+        strike: string;
+}
