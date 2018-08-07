25358ad5006222dba6c8242a5652be019fbf36df
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-08-07 20:57:04 -0300

    refactor, add component calendar butterfly OK!

--- src/app/abas/calendar-butterfly/calendar-butterfly.component.ts
+++ src/app/abas/calendar-butterfly/calendar-butterfly.component.ts
@@ -1,15 +1,113 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CalendarButerfly } from '../../models/calendar-butterfly';
 
 @Component({
-  selector: 'app-calendar-butterfly',
+  // tslint:disable-next-line:component-selector
+  selector: 'calendar-butterfly',
   templateUrl: './calendar-butterfly.component.html',
   styleUrls: ['./calendar-butterfly.component.scss']
 })
 export class CalendarButterflyComponent implements OnInit {
+  @Input() dados;
+
+  dados_aux: CalendarButerfly[] = [];
+  serie = 'H/I';
+  strikes: any[] = [];
+  montagens: any[] = [];
+  precosEntrada: any[] = [];
+  desmontagens: any[] = [];
+  precosSaida: any[] = [];
+  parametroMontagem: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.filtrarSerie(this.dados);
+    this.filtros();
+    console.log('Dados: ', this.dados);
+  }
+
+  private filtros() {
+    this.filtrarStrike(this.dados_aux);
+    this.filtrarMontagem(this.dados_aux);
+    this.filtrarPrecoAtual(this.dados_aux);
+    this.filtrarDesmontagem(this.dados_aux);
+    this.filtrarPrecosSaida(this.dados_aux);
+    this.filtrarParametroMontagem(this.dados_aux);
+
+    console.log('Dados Filtrados: ', this.dados_aux);
+  }
+
+  filtrarSerie(d) {
+    d.filter(dados => {
+      if (this.serie === dados.Serie) {
+        this.dados_aux.push(dados);
+      }
+    });
+  }
+
+  filtrarStrike(strike) {
+        strike.filter(s => {
+          this.strikes.push(s.asa_inferior.Strike);
+          this.strikes.push(s.miolo.Strike);
+          this.strikes.push(s.asa_superior.Strike);
+        });
+  }
+
+  filtrarMontagem(montagem) {
+    montagem.filter(m => {
+      this.montagens.push('+ 1 x' + m.asa_inferior.Valor_Bid);
+      this.montagens.push('- 2 x' + m.miolo.Valor_Ask);
+      this.montagens.push('+ 1 x' + m.asa_superior.Valor_Bid);
+    });
+  }
+
+  filtrarPrecoAtual(preco: CalendarButerfly[]) {
+    preco.filter(p => {
+      this.precosEntrada.push(p.preco_entrada_atual);
+    });
+  }
+
+  filtrarDesmontagem(desmontagem) {
+    desmontagem.filter(des => {
+      this.desmontagens.push('-1 x' + des.asa_inferior.Valor_Ask);
+      this.desmontagens.push('+ 2 x' + des.miolo.Valor_Bid);
+      this.desmontagens.push('- 1 x' + des.asa_superior.Valor_Ask);
+    });
+  }
+
+  filtrarPrecosSaida(preco: CalendarButerfly[]) {
+    preco.filter(pre => {
+      this.precosSaida.push(pre.preco_saida_atual);
+    });
+  }
+
+  filtrarParametroMontagem(pamMontagem: CalendarButerfly[]) {
+    pamMontagem.filter(pam => {
+      this.parametroMontagem.push(pam.preco_entrada_parametro);
+    });
+  }
+
+  changeSerie(e) {
+    this.dados_aux = [];
+    this.limpaDados();
+    this.dados.filter(d => {
+      if (d.Serie === e.value) {
+        this.dados_aux.push(d);
+      }
+    });
+
+    this.filtros();
+    // console.log('Filtado pelo radio: ', this.dados_aux);
+  }
+
+  private limpaDados() {
+    this.strikes = [];
+    this.montagens = [];
+    this.precosEntrada = [];
+    this.desmontagens = [];
+    this.precosSaida = [];
+    this.parametroMontagem = [];
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,7 @@ import { CalendarButerfly } from './models/calendar-butterfly';
 export class AppComponent implements OnInit {
 
   dados: CalendarButerfly[];
-  dados_aux: CalendarButerfly[] = [];
-  serie = 'H/I';
-  strikes: any[] = [];
-  montagens: any[] = [];
-  precosEntrada: any[] = [];
-  desmontagens: any[] = [];
-  precosSaida: any[] = [];
-  parametroMontagem: any[] = [];
+  aba = '';
 
 
   constructor(public servicos: ServicosService) {
@@ -42,99 +35,13 @@ export class AppComponent implements OnInit {
 
 
   calendarButerfly() {
-  this.limpaDados();
     this.servicos.getCalendarButerfly()
     .subscribe((d: CalendarButerfly[]) => {
       this.dados = d; // All
-    //  this.serie = d[0].Serie;
-      console.log('Dados: ', this.dados);
-      console.log('Serie: ', this.serie);
-      this.filtrarSerie(d);
-      console.log('Dados filtrados: ', this.dados_aux);
-      this.filtros();
+      this.aba = 'Calendar Butterfly';
     });
   }
 
-  private filtros() {
-    this.filtrarStrike(this.dados_aux);
-    this.filtrarMontagem(this.dados_aux);
-    this.filtrarPrecoAtual(this.dados_aux);
-    this.filtrarDesmontagem(this.dados_aux);
-    this.filtrarPrecosSaida(this.dados_aux);
-    this.filtrarParametroMontagem(this.dados_aux);
-  }
-
-  filtrarSerie(d) {
-    d.filter(dados => {
-      if (this.serie === dados.Serie) {
-        this.dados_aux.push(dados);
-      }
-    });
-  }
-
-  filtrarStrike(strike) {
-        strike.filter(s => {
-          this.strikes.push(s.asa_inferior.Strike);
-          this.strikes.push(s.miolo.Strike);
-          this.strikes.push(s.asa_superior.Strike);
-        });
-  }
-
-  filtrarMontagem(montagem) {
-    montagem.filter(m => {
-      this.montagens.push('+ 1 x' + m.asa_inferior.Valor_Bid);
-      this.montagens.push('- 2 x' + m.miolo.Valor_Ask);
-      this.montagens.push('+ 1 x' + m.asa_superior.Valor_Bid);
-    });
-  }
-
-  filtrarPrecoAtual(preco: CalendarButerfly[]) {
-    preco.filter(p => {
-      this.precosEntrada.push(p.preco_entrada_atual);
-    });
-  }
-
-  filtrarDesmontagem(desmontagem) {
-    desmontagem.filter(des => {
-      this.desmontagens.push('-1 x' + des.asa_inferior.Valor_Ask);
-      this.desmontagens.push('+ 2 x' + des.miolo.Valor_Bid);
-      this.desmontagens.push('- 1 x' + des.asa_superior.Valor_Ask);
-    });
-  }
-
-  filtrarPrecosSaida(preco: CalendarButerfly[]) {
-    preco.filter(pre => {
-      this.precosSaida.push(pre.preco_saida_atual);
-    });
-  }
-
-  filtrarParametroMontagem(pamMontagem: CalendarButerfly[]) {
-    pamMontagem.filter(pam => {
-      this.parametroMontagem.push(pam.preco_entrada_parametro);
-    });
-  }
-
-  changeSerie(e) {
-    this.dados_aux = [];
-    this.limpaDados();
-    this.dados.filter(d => {
-      if (d.Serie === e.value) {
-        this.dados_aux.push(d);
-      }
-    });
-
-    this.filtros();
-    // console.log('Filtado pelo radio: ', this.dados_aux);
-  }
-
-  private limpaDados() {
-    this.strikes = [];
-    this.montagens = [];
-    this.precosEntrada = [];
-    this.desmontagens = [];
-    this.precosSaida = [];
-    this.parametroMontagem = [];
-  }
 
 
 }
