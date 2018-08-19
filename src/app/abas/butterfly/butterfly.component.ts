7bc77946bd4f44bfff20cdee4cc010dc967384f9
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-08-19 15:01:18 -0300

    add filtro no butterfly

--- src/app/abas/butterfly/butterfly.component.ts
+++ src/app/abas/butterfly/butterfly.component.ts
@@ -12,105 +12,31 @@ export class ButterflyComponent implements OnInit {
   @Input() dados;
 
   dados_aux: Butterfly[] = [];
-  serie = 'H';
-  strikes: any[] = [];
-  montagens: any[] = [];
-  precosEntrada: any[] = [];
-  desmontagens: any[] = [];
-  precosSaida: any[] = [];
-  parametroMontagem: any[] = [];
+  serie = 'I';
 
   constructor() { }
 
   ngOnInit() {
-    // this.filtrarSerie(this.dados);
-    // this.filtros();
-    console.log('Dados Butterfly: ', this.dados);
-    this.dados_aux =  this.dados;
-  }
 
-  private filtros() {
-    this.filtrarStrike(this.dados_aux);
-    this.filtrarMontagem(this.dados_aux);
-    this.filtrarPrecoAtual(this.dados_aux);
-    this.filtrarDesmontagem(this.dados_aux);
-    this.filtrarPrecosSaida(this.dados_aux);
-    this.filtrarParametroMontagem(this.dados_aux);
+    console.log('Dados Butterfly: ', this.dados[0]); // I
+    this.dados_aux =  this.dados[0];
 
-    console.log('Dados Filtrados Butterfly: ', this.dados_aux);
-  }
+    console.log('todas séries: ', this.dados);
 
-  filtrarSerie(d) {
-    d.filter(dados => {
-      if (this.serie === dados.Serie) {
-        this.dados_aux.push(dados);
-      }
-    });
-  }
-
-  filtrarStrike(strike) {
-        strike.filter(s => {
-          this.strikes.push(s.asa_inferior.Strike);
-          this.strikes.push(s.miolo.Strike);
-          this.strikes.push(s.asa_superior.Strike);
-        });
-  }
-
-  filtrarMontagem(montagem) {
-    montagem.filter(m => {
-      this.montagens.push('+ 1 x' + m.asa_inferior.Valor_Bid);
-      this.montagens.push('- 2 x' + m.miolo.Valor_Ask);
-      this.montagens.push('+ 1 x' + m.asa_superior.Valor_Bid);
-    });
-  }
-
-  filtrarPrecoAtual(preco: Butterfly[]) {
-    preco.filter(p => {
-      this.precosEntrada.push(p.preco_entrada_atual);
-    });
-  }
-
-  filtrarDesmontagem(desmontagem) {
-    desmontagem.filter(des => {
-      this.desmontagens.push('-1 x' + des.asa_inferior.Valor_Ask);
-      this.desmontagens.push('+ 2 x' + des.miolo.Valor_Bid);
-      this.desmontagens.push('- 1 x' + des.asa_superior.Valor_Ask);
-    });
-  }
-
-  filtrarPrecosSaida(preco: Butterfly[]) {
-    preco.filter(pre => {
-      this.precosSaida.push(pre.preco_saida_atual);
-    });
-  }
-
-  filtrarParametroMontagem(pamMontagem: Butterfly[]) {
-    pamMontagem.filter(pam => {
-      this.parametroMontagem.push(pam.preco_entrada_parametro);
-    });
   }
 
   changeSerie(e) {
+    this.serie = e.value;
     this.dados_aux = [];
-    this.limpaDados();
-    this.dados.filter(d => {
-      if (d.Serie === e.value) {
-        this.dados_aux.push(d);
-      }
-    });
 
-    this.filtros();
-    // console.log('Filtado pelo radio: ', this.dados_aux);
+    if (this.serie === 'I') {
+      this.dados_aux = this.dados[0];
+    } else if (this.serie === 'J') {
+      this.dados_aux =  this.dados[1];
+    } else if (this.serie === 'K') {
+      this.dados_aux = this.dados[2];
+    }
+
   }
-
-  private limpaDados() {
-    this.strikes = [];
-    this.montagens = [];
-    this.precosEntrada = [];
-    this.desmontagens = [];
-    this.precosSaida = [];
-    this.parametroMontagem = [];
-  }
-
 
 }
